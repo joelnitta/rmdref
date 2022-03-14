095f983056ce10c5721e1b1fc61f0378db7168b8
Joel Nitta <joelnitta@gmail.com>
2022-03-14 11:32:46 +0900

    Add extract_citations()

---
+++ R/extract_citations.R
@@ -0,0 +1,33 @@
+#' Extract citations (formatted like `[@key]`) from an Rmd file
+#'
+#' @param rmd_file Character vector; path to Rmd file
+#'
+#' @return Dataframe (tibble) with one column 'key'
+#' @export
+#' @autoglobal
+#' @examples
+#' tempfile <- tempfile(fileext = ".Rmd")
+#' lines <- c(
+#'   "---",
+#'   "title: 'Report'",
+#'   "output_format: html_document",
+#'   "---",
+#'   "This statement is supported by @Nitta2021",
+#'   "[@Foo1983; @Blah2001; but see @Smith2020]."
+#' )
+#' write(lines, tempfile)
+#' extract_citations(tempfile)
+#' file.remove(tempfile)
+extract_citations <- function(rmd_file) {
+  readr::read_lines(rmd_file) %>%
+    stringr::str_split(" |;") %>%
+    unlist %>%
+    magrittr::extract(., stringr::str_detect(., "@")) %>%
+    stringr::str_remove_all("^[^@]*") %>%
+    stringr::str_remove_all('\\[|\\]|\\)|\\(|\\.$|,|\\{|\\}|\\\\|\\"') %>%
+    magrittr::extract(., stringr::str_detect(., "^@|^-@")) %>%
+    stringr::str_remove_all("^@|^-@") %>%
+    unique %>%
+    sort %>%
+    tibble::tibble(key = .)
+}
